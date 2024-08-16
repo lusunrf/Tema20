9f992cba96c5e99b9aae127c07c9fbf53bf45868
FIRST_NAME LAST_NAMERadu <MY_NAME@example.comlusunrf@hotmail.com>
2024-08-16 17:14:05 +0300

    commit 6 pe branch dezvoltare, modificare main2

--- Folder2/main2.py
+++ Folder2/main2.py
@@ -1,5 +1,5 @@
 
 
 import matematica2 as mate
-print("impartirea numerelor 6 si 3 este: ",mate.impartire(6,3))
-print("adunarea numerelor 6 si 3 este: ",mate.adunare(6,3))
+print("impartirea numerelor 6 si 3 este: ",mate.impartire(9,3))
+print("adunarea numerelor 6 si 3 este: ",mate.adunare(9,3))
